9ed6d571abaa6753972a9de7e69faad88391fbe1
Murat Sahin <78914293+muhendisoluyorumben@users.noreply.github.com>
2021-05-05 13:48:40 +0300

    Update StudentManager.java

--- inheritanceOdev/StudentManager.java
+++ inheritanceOdev/StudentManager.java
@@ -1,9 +1,9 @@
-package inheritanceOdev;
+
 
 public class StudentManager extends UserManager{
 	User user=new User();
 	public void add(User user) {
 		this.user=user;
-		System.out.println("Öğrenci eklendi "+user.getName());
+		System.out.println("Ã–Ã°renci eklendi "+user.getName());
 	}
 }

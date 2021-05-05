188f29010d663a76fc28c8e9815699f2d672b088
Murat Sahin <78914293+muhendisoluyorumben@users.noreply.github.com>
2021-05-05 13:48:00 +0300

    Update InstructorManager.java

--- inheritanceOdev/InstructorManager.java
+++ inheritanceOdev/InstructorManager.java
@@ -1,9 +1,9 @@
-package inheritanceOdev;
+
 
 public class InstructorManager extends UserManager{
 	User user=new User();
 	public void add(User user) {
 		this.user=user;
-		System.out.println("Öðretmen eklendi "+user.getName());
+		System.out.println("Ã–Ã°retmen eklendi "+user.getName());
 	}
 }

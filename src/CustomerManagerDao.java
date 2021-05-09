440aebcd3f3fcbc2f7584e0c0f52601c8746254d
Murat Sahin <78914293+muhendisoluyorumben@users.noreply.github.com>
2021-05-09 17:34:39 +0300

    Delete src directory

--- src/CustomerManagerDao.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.RemoteException;
-
-public interface CustomerManagerDao {
-
-	public void addCustomer(Customer customers) throws RemoteException;
-	public void signInCustomer(Customer customer);
-	public Customer signOutCustomer(Customer customer);
-}
--- src/DatabaseManagerDao.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.RemoteException;
-
-public interface DatabaseManagerDao { // Her manager ýn bir interface i olmak zorunda. ilerde kullanýlacak.
-	public void addDatabase(Customer customer)throws RemoteException;
-}

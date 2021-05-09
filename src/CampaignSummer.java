4917e8222ad69d2882eaa01851f6ca8fbc918ee1
Murat Sahin <78914293+muhendisoluyorumben@users.noreply.github.com>
2021-05-09 17:40:49 +0300

    Delete src directory

--- src/CampaignSummer.java
+++
@@ -1,9 +0,0 @@
-
-public class CampaignSummer extends CampaignBase{
-
-	public CampaignSummer(String campaignName, double d) {
-		super(campaignName, d);
-		
-	}
-
-}
--- src/CampaignWinter.java
+++
@@ -1,9 +0,0 @@
-
-public class CampaignWinter extends  CampaignBase{
-
-	public CampaignWinter(String campaignName, double discount) {
-		super(campaignName, discount);
-		// TODO Auto-generated constructor stub
-	}
-
-}
